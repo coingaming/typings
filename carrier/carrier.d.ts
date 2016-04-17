4ae54f7b217bd05e8fcc039165f76de441f7053f
Reio <reio@heathmont.net>
2016-04-17 20:35:35 +0300

    Update carrier.d.ts

--- carrier/carrier.d.ts
+++ carrier/carrier.d.ts
@@ -1,3 +1,3 @@
 import * as tls from 'tls';
 import * as events from 'events';
-export function carry(conn: tls.ClearTextStream, callback?: Function): events.EventEmitter;
+export function carry(conn: tls.ClearTextStream, callback?: Function, encoding?: string, delim?: string): events.EventEmitter;
